07a8c8259c0b42a8456f9a2a3ce977d2227093fe
Tam Nguyen <teresakhanhtam@gmail.com>
2024-12-06 03:00:52 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Exam from './Components/Exam/Exam'
+
+const App = () => {
+  return (
+    <>
+      <Exam/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/data.js
@@ -0,0 +1,26 @@
+export const data = [
+    {
+      question: "Choose the best answer about software type.",
+      option1: "There are many different application types, each requires different appropriate techniques, methods and development tools.",
+      option2: "All of the other answers are correct.",
+      option3: "Business and society are always changing, we continuously need new techniques, methods, tools in software development.",
+      option4: " Software development should be performed in considering about platforms and computer/device types.",
+      ans: 2,
+    },
+    {
+      question: "Choose the best answer about people in software engineering.",
+      option1: "None of the other answers are correct.",
+      option2: "Software developer has the responsibility on customer care, software design and software testing.",
+      option3: "Software designer is not an engineering job as it uses no theory.",
+      option4: "Programmer = coder + software tester.",
+      ans: 1,
+    },
+    {
+      question: "Which of the following is/are not software engineer responsibilities?",
+      option1: "Involves wider responsibilities than simply the application of technical skills.",
+      option2: "Make sure that the software products are as professional as possible.",
+      option3: "Be fair and take action not to discriminate.",
+      option4: "Satisfy customer requirements in all circumstances.",
+      ans: 4,
+    },
+  ];
